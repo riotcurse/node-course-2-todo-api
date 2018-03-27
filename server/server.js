a44f8da65ca61f4bc7d7dff03e5591a36b737cf7
Rio Kurz <riotkurz@iMac13.local>
2018-03-27 07:25:30 -0500

    POST /users route added

--- server/server.js
+++ server/server.js
@@ -96,6 +96,21 @@ app.patch('/todos/:id',(req,res)=>{
   })
 });
 
+//POST /users
+app.post('/users',(req,res)=>{
+  console.log(req.body);
+
+  var body = _.pick(req.body,['email','password'])
+  var user = new User(body);
+
+  user.save().then((user)=>{
+    res.send(user);
+  },(e)=>{
+    res.status(400).send(e);
+  });
+
+});
+
 app.listen(port, ()=>{
   console.log(`Started on port ${port}`)
 });
